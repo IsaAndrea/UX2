00afdd926f02609aec4f6fc7dc1715c131c1e878
isaAndrea <isamar.hernandez@usach.cl>
2019-01-16 06:57:31 -0300

    footer

--- Front/src/Menu.vue
+++ Front/src/Menu.vue
@@ -17,6 +17,43 @@
       <router-view></router-view>
     </main>
 
+    <footer>
+        <div class="columnaFooter">
+          <p>Nuestra empresa</p>
+            <ul>
+              <li>Conócenos</li>
+              <li>Sala de Prensa</li>
+              <li>VTR Internet Segura</li>
+              <li>Zona de Proveedores</li>
+              <li>Inmobiliaria</li>
+              <li>Lugares y formas de Pago</li>
+              <li>Carrier 111</li>
+            </ul>
+          </div>  
+            
+        <div class="columnaFooter">
+          <p>Términos y regulaciones</p>
+            <ul>
+              <li>Contrato de servicios VTR</li>
+              <li>Cotizador de Servicios</li>
+              <li>Liberacion de equipos</li>
+              <li>Multibanda SAE - Consulta de IMEI</li>
+              <li>Nueva normativa de móviles</li>
+              <li>Sistema de Alerta Emergencias (SAE)</li>
+              <li>Ley de Neutralidad</li>
+            </ul>
+        </div>  
+             
+        <div class="columnaFooterEn">
+          <p>Entretención</p>
+            <ul>
+              <li>Canales HD</li>
+              <li>VTR Play</li>
+              <li>Concursos</li>
+            </ul>
+        </div> 
+        </footer>
+
     
 
 </div>
