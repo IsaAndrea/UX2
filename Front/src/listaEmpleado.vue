35feb9d8785209ddeee146e33fabe366a362516b
Unknown <oscar.utreras@usach.cl>
2019-01-19 15:19:59 -0300

    Filter

--- Front/src/listaEmpleado.vue
+++ Front/src/listaEmpleado.vue
@@ -6,6 +6,20 @@
 
         <h1>{{title}}</h1>
 
+        <select v-model="selected">
+            <option disabled value="">Seleccione una opción</option>
+            <option>Todos</option>
+            <option>Internet Hogar</option>
+            <option>Telefonía Hogar</option>
+            <option>Televisión Hogar</option>
+            <option>Internet Móvil</option>
+            <option>Telefonía Móvil</option>
+            <option>Resuelto</option>
+            <option>Pendiente</option>
+        </select>
+
+        <button v-on:click="filter">Filtrar</button>
+
         <div class="filtros">
             <input class="filtroT" type="radio" value="todos" checked="checked" /> Todos
             <div class="filtroEstado">
@@ -16,11 +30,11 @@
             </div>
             <div class="filtroCategoria">
                 <label>Filtrar por Categoría</label>
-                <div class="filtro"> <input type="radio" /> Internet Hogar </div>
-                <div class="filtro"> <input type="radio" /> Internet Móvil </div>
-                <div class="filtro"><input type="radio" /> Telefonía Hogar </div>
-                <div class="filtro"> <input type="radio" /> Telefonía Móvil </div>
-                <div class="filtro"> <input type="radio" /> Televisión </div>
+                <div class="filtro"> <input type="radio" />Internet Hogar</div>
+                <div class="filtro"> <input type="radio" />Internet Móvil</div>
+                <div class="filtro"><input type="radio" />Telefonía Hogar</div>
+                <div class="filtro"> <input type="radio" />Telefonía Móvil</div>
+                <div class="filtro"> <input type="radio" />Televisión</div>
             </div>
         </div>
 
@@ -67,10 +81,12 @@
     data(){
         return{
         title:'Reclamos',
-        claims: [{reason: 'Cobro indebido', date: '22/10/2018', category: 'Telefonía Móvil'},
-                {reason: 'Televisión por cable sin señal', date: '23/11/2018', category: 'Hogar'},
-                {reason: 'Teléfono sin tono', date: '22/12/2018', category: 'Telefonía'},
-                {reason: 'Lentitud en bajadas', date: '24/12/2018', category: 'Internet'}]
+        claims: [{reason: 'Cobro indebido', date: '22/10/2018', category: 'Telefonía Móvil', state: 'Resuelto'},
+                {reason: 'Televisión por cable sin señal', date: '23/11/2018', category: 'Hogar', state: 'Pendiente'},
+                {reason: 'Teléfono sin tono', date: '22/12/2018', category: 'Telefonía Hogar', state: 'Pendiente'},
+                {reason: 'Lentitud en bajadas', date: '24/12/2018', category: 'Internet Hogar', state: 'Pendiente'}],
+        selected: 'Todos',
+        claimSelected: []
         }
     },
     methods:{
@@ -78,9 +94,32 @@
         visualizar(){
             window.location.href = '#/gestionD'
         },
+        filter(){
+            if(this.selected == "Todos"){
+                this.claimSelected = this.claims
+            }
+            else{
+                this.claimSelected = []
+                if(this.selected == 'Resuelto' || this.selected == 'Pendiente'){
+                    this.claims.forEach(element => {
+                        if(element.state == this.selected){
+                            this.claimSelected.push(element)
+                        }
+                    });
+                }
+                else{
+                    this.claims.forEach(element => {
+                        if(element.category == this.selected){
+                            this.claimSelected.push(element)
+                        }
+                    });
+                }
+            }
+        }
     },
     mounted:function(){
         console.log('listado.vue')
+        this.claimSelected = this.claims
     }
     }
 </script>
